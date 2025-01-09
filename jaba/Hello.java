40c738761cf757b12932cca12111c05f0d5cc202
gureisu81 <20f02581@gmail.com>
2025-01-09 09:35:01 +0900

    Add files via upload

---
+++ jaba/Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+    public static void main(String[] args){
+       for(int i = 0; i < 10; i++){
+        if (i<4){System.out.println("おはよう");}
+        else if (i<12){System.out.println("こんにちは");}
+       
+     
+
+    }
+
+}
+}
